da49777407b1e6fc1d749a5856110a020f94338f
Nomusa Mtshali <156771602+nomusamtshali@users.noreply.github.com>
2025-10-14 15:31:08 +0200

    commit

--- .next/static/webpack/webpack.8c6cf73d0ed4bb8a.hot-update.js
+++ .next/static/webpack/webpack.8c6cf73d0ed4bb8a.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "55431ba250a8bf3e"; }
+/******/ 	__webpack_require__.h = function() { return "7230a5b755cd29e0"; }
 /******/ }();
 /******/ 
 /******/ }
